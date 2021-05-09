d0bdb28d761c9b088e968350053bcb64b0492054
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-05-09 16:02:19 +0900

    create.html.erbの削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to products_path
     else
       render action: :new
     end
