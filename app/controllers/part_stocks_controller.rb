5b4deb80a9e2dab7d0b21354a2159b9bf7334915
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-05-18 20:06:54 +0900

    部品発注画面

--- app/controllers/part_stocks_controller.rb
+++ app/controllers/part_stocks_controller.rb
@@ -1,2 +1,15 @@
 class PartStocksController < ApplicationController
+  def index
+    @part = Part.find(params[:id])
+  end
+end
+
+private
+
+def part_params
+  params.require(:part).permit(:name, :number, :category_id, :material, :price, :supplier_id, :image).merge(user_id: current_user.id)
+end
+
+def stock_form_params
+  params.require(:stock_form).permit(:name, :number, :category_id, :material, :price, :supplier_id,:still_extant, :lot, :buy_point).merge(user_id: current_user.id)
 end
